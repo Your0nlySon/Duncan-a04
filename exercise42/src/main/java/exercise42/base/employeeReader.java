de80f80e99ca29504f093eb555f3fb2e0c431077
Your0nlySon <eaduncan18@gmail.com>
2021-10-13 22:54:12 -0400

    Logic Code

--- exercise42/src/main/java/exercise42/base/employeeReader.java
+++ exercise42/src/main/java/exercise42/base/employeeReader.java
@@ -1,6 +1,8 @@
 package exercise42.base;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,10 +10,24 @@ public class employeeReader {
 
     public void rEmployee(File file, Scanner read, List<List<String>> employee) {
         //try
-            //call read and then scan the file
-            //read line from file and add it to a string array but split it by ,
-            //add the result to the arrayList employee
         //catch
             //stackTrace
+        try {
+            //call read and then scan the file
+            read = new Scanner(file);
+
+            while (read.hasNext()) {
+                //read line from file and add it to a string array but split it by ,
+                String line = read.next();
+                String[] values = line.split(",");
+                //add the result to the arrayList employee
+                employee.add(Arrays.asList(values));
+            }
+            read.close();
+        } catch (FileNotFoundException e) {
+            //catch
+            //stackTrace
+            e.printStackTrace();
+        }
     }
 }
